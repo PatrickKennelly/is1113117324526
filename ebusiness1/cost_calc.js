8e5aa634c6936c426a7e1a57c7f74edddc7758fc
Patrick Kennelly <117324526@umail.ucc.ie>
2018-02-23 13:00:48 +0000

    adding extra options to ebusiness

--- ebusiness1/cost_calc.js
+++ ebusiness1/cost_calc.js
@@ -4,11 +4,21 @@ function calcSub(){
     
     var argSubTotal;
     
-    if(document.getElementById('salesforce').checked) {
+    if (document.getElementById('salesforce').checked) {
       argSubTotal = 100;
     }
-    else {
+    
+    else if (document.getElementById('cloud9').checked {
+      argSubTotal = 200;
+    }
+    
+    else if (document.getElementById('aws').checked {
       argSubTotal = 300;
+    }
+    
+    else{
+      
+      argSubTotal = 400;
     }
     
     display(argSubTotal);
